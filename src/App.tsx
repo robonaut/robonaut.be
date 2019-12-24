94f064758851b9ff69010164cb3d4732fb4ef354
Ilja Strobbe <ilja@robonaut.be>
2019-12-24 11:55:50 +0100

    Use <GlobalStyle />

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { renderToString } from 'react-dom/server';
 import { ServerStyleSheet } from 'styled-components';
 
 import { routeDefinitions } from './routes';
+import GlobalStyle from './style/global';
 import { MainContainer } from './style/layout';
 
 const App: React.StatelessComponent<{}> = () => (
@@ -22,6 +23,7 @@ export function renderStatic(url: string): { style: string; body: string } {
   const sheet = new ServerStyleSheet();
   const body = renderToString(
     <ServerLocation url={url}>
+      <GlobalStyle />
       <App />
     </ServerLocation>
   );
