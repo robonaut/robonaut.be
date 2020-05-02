9fdaf0c9c8b31e0e1b36a189e4a7312a4b14f855
Ilja Strobbe <ilja@robonaut.be>
2020-05-02 11:28:07 +0200

    Lint autofix

--- src/pages/renderers/Code.tsx
+++ src/pages/renderers/Code.tsx
@@ -1,6 +1,5 @@
-import React, { useEffect, useState } from 'react';
-
 import Prism from 'prismjs';
+import React, { useEffect, useState } from 'react';
 
 export default ({
   text,
@@ -11,10 +10,12 @@ export default ({
 }): JSX.Element => {
   const className = `language-${lang}`;
   const [html, setHtml] = useState('');
+
   useEffect(() => {
     // This solves an issue with incorrect rehydration from the server
     setHtml(Prism.highlight(text, Prism.languages.javascript, 'javascript'));
   }, []);
+
   return (
     <pre className={className}>
       <code className={className} dangerouslySetInnerHTML={{ __html: html }} />
