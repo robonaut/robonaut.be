799f6a2d1cd965766087d834a4b3f8dc258205d0
Ilja Strobbe <ilja@robonaut.be>
2020-05-24 18:30:58 +0200

    Decrease font-size on smaller devices

--- src/style/global.ts
+++ src/style/global.ts
@@ -11,10 +11,10 @@ const GlobalStyle = createGlobalStyle`
   }
   :root {
     font-family: 'Open Sans', sans-serif;
-    font-size: ${px2vw(32, 768)};
+    font-size: ${px2vw(24, 768)};
 
     @media (min-width: 768px) {
-      font-size: ${px2vw(24, 768)};
+      font-size: ${px2vw(18, 768)};
     }
 
     @media (min-width: 1024px) {
