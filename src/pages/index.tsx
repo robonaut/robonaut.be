125f21fe4debbebe03d46ffbb533ff2b0193d06d
Ilja Strobbe <ilja@robonaut.be>
2020-06-08 12:12:13 +0200

    chore: Update dependencies

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,12 +11,14 @@ const Components: Record<string, JSX.Element> = {
 
 if (!process.env.IS_WEBPACK) {
   Object.keys(Components).forEach((key) => {
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
     const markdown = require(`./markdown/${key}.md`).default;
 
     Components[key] = render(markdown);
   });
 } else {
   Object.keys(Components).forEach((key) => {
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
     const markdown = require('fs')
       .readFileSync(`${__dirname}/markdown/${key}.md`)
       .toString();
